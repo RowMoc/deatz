daf1d3b87d86c3e7ee2e1a91e76333e6f351657d
Rowan Mockler <rowanmockelr@gmail.com>
2019-11-13 17:03:03 -0800

    First commit

---
+++ Deatz/Deatz/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Deatz
+//
+//  Created by Rowan Mockler on 2019/11/13.
+//  Copyright © 2019 Deatz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Deatz/Deatz/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Deatz
+//
+//  Created by Rowan Mockler on 2019/11/13.
+//  Copyright © 2019 Deatz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

85fe11d6187243e385203cbfd7394b934aaf103d
Elina Thadhani <elina.thadhani17@gmail.com>
2019-11-20 00:28:56 -0800

    set up of nav bar

---
+++ Deatz/Deatz/NavBar.swift
@@ -0,0 +1,30 @@
+//
+//  NavBar.swift
+//  Deatz
+//
+//  Created by Elina Thadhani on 11/19/19.
+//  Copyright © 2019 Deatz. All rights reserved.
+//
+
+import UIKit
+
+class NavBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
